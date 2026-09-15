67fd8385fe8909604935d47e90769a253dcdb698
watchtvsportsinc-spec <watchtvsportsinc@gmail.com>
2026-09-14 23:45:18 -0400

    Tighten robots rules for public V2 routes

--- app/robots.ts
+++ app/robots.ts
@@ -2,12 +2,11 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-      },
-    ],
+    rules: [{
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/api/", "/favorites"],
+    }],
     sitemap: "https://watchtvsport.com/sitemap.xml",
     host: "https://watchtvsport.com",
   };
