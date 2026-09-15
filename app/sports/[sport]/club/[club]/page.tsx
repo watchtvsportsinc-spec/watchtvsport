dcdfcfbf8ea04522f77c239c204c8cd1b2b462af
watchtvsportsinc-spec <watchtvsportsinc@gmail.com>
2026-09-15 16:12:16 -0400

    Add universal sport club route

---
+++ app/sports/[sport]/club/[club]/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+import UniversalClubProfilePage, { buildUniversalClubMetadata } from "@/components/UniversalClubProfilePage";
+
+type PageProps = { params: Promise<{ sport: string; club: string }> };
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const { sport, club } = await params;
+  return buildUniversalClubMetadata(sport, club);
+}
+
+export default async function ClubPage({ params }: PageProps) {
+  const { sport, club } = await params;
+  return <UniversalClubProfilePage sport={sport} club={club} />;
+}
