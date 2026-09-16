9bad2dc1a586d027fea0f3ba78629e90cc828eca
watchtvsportsinc-spec <watchtvsportsinc@gmail.com>
2026-09-16 15:19:22 -0400

    SEO: let Google crawl noindex utility pages

--- app/robots.ts
+++ app/robots.ts
@@ -17,7 +17,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/api/", "/favorites"],
+        disallow: ["/api/"],
       },
     ],
     sitemap: "https://watchtvsport.com/sitemap.xml",
