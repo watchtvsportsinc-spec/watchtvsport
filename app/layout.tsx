a9af0377174b0a86697595fb384794dda7df2faa
watchtvsportsinc-spec <watchtvsportsinc@gmail.com>
2026-09-17 08:15:03 -0400

    Load home V3 layout refinements

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import "./match-ux-compact.css";
 import "./broadcaster-logos.css";
 import "./home-favorites-compact.css";
 import "./header-v2.css";
+import "./home-v3-tweaks.css";
 import { Analytics } from "@vercel/analytics/next";
 import Script from "next/script";
 import HeaderNav from "./HeaderNav";
