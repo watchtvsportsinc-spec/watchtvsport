ff3604cebd3983587811de23c29885afd9ae9df1
watchtvsportsinc-spec <watchtvsportsinc@gmail.com>
2026-09-16 20:30:19 -0400

    Load compact match UX overrides last

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import "./home-premium.css";
 import "./home-premium-visual.css";
 import "./seo-crawl.css";
 import "./mobile-density.css";
+import "./match-ux-compact.css";
 import { Analytics } from "@vercel/analytics/next";
 import Script from "next/script";
 import HeaderNav from "./HeaderNav";
