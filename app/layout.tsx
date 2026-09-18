f51ff69c28a65803d8501fef60c0c4ec3af8a919
watchtvsportsinc-spec <watchtvsportsinc@gmail.com>
2026-09-17 21:08:23 -0400

    Load progressive home discovery styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ import "./home-v3-tweaks.css";
 import "./home-mobile-final.css";
 import "./home-schedule-cleanup.css";
 import "./mobile-header-final.css";
+import "./home-discovery.css";
 import { Analytics } from "@vercel/analytics/next";
 import Script from "next/script";
 import HeaderNav from "./HeaderNav";
