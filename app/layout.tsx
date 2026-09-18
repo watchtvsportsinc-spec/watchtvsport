10dfbf9142ed4aa8de62c207146b67f553f85406
watchtvsportsinc-spec <watchtvsportsinc@gmail.com>
2026-09-18 00:02:55 -0400

    Load event detail styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ import "./home-mobile-final.css";
 import "./home-schedule-cleanup.css";
 import "./mobile-header-final.css";
 import "./home-discovery.css";
+import "./event-detail.css";
 import { Analytics } from "@vercel/analytics/next";
 import Script from "next/script";
 import HeaderNav from "./HeaderNav";
